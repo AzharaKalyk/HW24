e69e091bd8079c14fe27620c47b6d24448031fb5
AzharaKalyk <kab_180495@mail.ru>
2023-10-09 23:18:04 +0600

    Initial Commit

---
+++ HW24/HW24App.swift
@@ -0,0 +1,17 @@
+//
+//  HW24App.swift
+//  HW24
+//
+//  Created by Azhar Kalyk on 09.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct HW24App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
